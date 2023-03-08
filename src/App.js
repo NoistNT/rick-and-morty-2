62cc8765529ae848a8113fdf4fa0a9cb1dadf044
NoistNT <arielgnr23@gmail.com>
2023-03-08 00:57:40 -0300

    Refactor data showed by App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React from 'react'
-import './App.css'
-import Card from './components/Card/Card.jsx'
 import Cards from './components/Cards/Cards.jsx'
 import SearchBar from './components/SearchBar/SearchBar.jsx'
-import characters, { Rick } from './data.js'
+import characters from './data.js'
+import { AppContainer } from './components/StyledComponents/App'
 
 export default function App () {
   return (
-    <div className="App" style={{ padding: '25px' }}>
-      <div>
-        <Card
-          name={Rick.name}
-          species={Rick.species}
-          gender={Rick.gender}
-          image={Rick.image}
-          onClose={() => window.alert('Emulamos que se cierra la card')}
-        />
-      </div>
-      <hr />
-      <div>
-        <Cards characters={characters} />
-      </div>
-      <hr />
+    <AppContainer>
       <div>
         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
       </div>
-    </div>
+      <div>
+        <Cards characters={characters} />
+      </div>
+    </AppContainer>
   )
 }
