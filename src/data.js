535907ea62c9f08b35b030b0c4dc153c42283504
NoistNT <arielgnr23@gmail.com>
2023-03-08 00:53:33 -0300

    Add local data to test components until API Fetch implementation

--- src/data.js
+++ src/data.js
@@ -1,5 +1,12 @@
 const characters = [
   {
+    id: 1,
+    name: 'Rick Sanchez',
+    species: 'Human',
+    gender: 'Male',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  },
+  {
     id: 2,
     name: 'Morty Smith',
     species: 'Human',
@@ -19,15 +26,35 @@ const characters = [
     species: 'Human',
     gender: 'Female',
     image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+  },
+  {
+    id: 5,
+    name: 'Jerry Smith',
+    species: 'Human',
+    gender: 'Male',
+    image: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg'
+  },
+  {
+    id: 6,
+    name: 'Albert Einstein',
+    species: 'Human',
+    gender: 'Male',
+    image: 'https://rickandmortyapi.com/api/character/avatar/11.jpeg'
+  },
+  {
+    id: 7,
+    name: 'Abradolf Lincler',
+    species: 'Human',
+    gender: 'Male',
+    image: 'https://rickandmortyapi.com/api/character/avatar/7.jpeg'
+  },
+  {
+    id: 8,
+    name: 'Adjudicator Rick',
+    species: 'Human',
+    gender: 'Male',
+    image: 'https://rickandmortyapi.com/api/character/avatar/8.jpeg'
   }
 ]
-
-export const Rick = {
-  id: 1,
-  name: 'Rick Sanchez',
-  species: 'Human',
-  gender: 'Male',
-  image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-}
 
 export default characters
