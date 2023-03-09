a5c8db3c154e246e554398a4ca66838f866c7e9c
NoistNT <arielgnr23@gmail.com>
2023-03-09 16:41:28 -0300

    Add styles for Nav and SearchBar

---
+++ src/components/StyledComponents/Nav.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const NavContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  background-color: #3c3649;
+`
+export const NavBarUl = styled.ul`
+  list-style: none;
+`
+export const NavBarLi = styled.li`
+  text-decoration-style: none;
+`
---
+++ src/components/StyledComponents/SearchBar.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const SearchBarContainer = styled.div`
+  padding: 1rem;
+`
+export const SearchBarInput = styled.input`
+  height: 1.5rem;
+  min-width: 25%;
+  outline: 0;
+  border: none;
+  border-radius: 0.15rem;
+  font-size: 1rem;
+  flex-grow: 1;
+`
+export const SearchBarButton = styled.button``
